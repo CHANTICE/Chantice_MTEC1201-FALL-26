9a7cbec7e7ae71f276eed99baf2c9047c6772d76
CHANTICE <chantice.fields13@login.cuny.edu>
2026-09-22 00:20:29 -0400

    commit

---
+++ Week3 Example/sketch.js
@@ -0,0 +1,31 @@
+/*
+Static v. Dynamic
+Introduction to 
+-Draw function
+-Basic input: mousex, mouseY
+*/
+
+//the code in setup block runs once
+
+function setup() 
+{
+  createCanvas(800, 600);
+
+  background (127, 127, 127); 
+
+}
+
+//the code in the draw block runs repeatedly
+function draw() 
+{
+  background(127, 127, 127);
+
+  //a STATIC line
+  line (0, 0, 400,300); //draws a line from the top-left corner of the canvs to the center of the canvas
+
+  //a DYNAMIC line
+  line ( 400, 300, mouseX, mouseY); //draws a line from the center of the canvas to the current mouse position
+
+  //ellips following mouse position
+  ellipse(mouseX, mouseY, 50, 50);  //draws an elips at the curren tmouse position with the width and heights of 50
+}
